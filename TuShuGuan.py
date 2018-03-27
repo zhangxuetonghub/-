1223c820929b7bb3a4a7142f74e69a5350735f60
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-03-27 02:32:36 +0000

    Update TuShuGuan.py

--- TuShuGuan.py
+++ TuShuGuan.py
@@ -1,12 +1,12 @@
 class Book:
     """图书类"""
 
-    def __init__(self, title, author, onreader='', waitreaders=[]):
+    def __init__(self, title, author):
         """构造一本书"""
         self._title = title
         self._author = author
-        self._onread = onreader
-        self._waitreaders = waitreaders
+        self._onread = ''
+        self._waitreaders = []
 
     def setreader(self, readername):
         """设置正在借阅的读者名字"""
@@ -38,11 +38,11 @@ class Book:
 class Patron:
     """读者类"""
 
-    def __init__(self, name, books=0, booklist=[]):
+    def __init__(self, name):
         """构造读者"""
         self._name = name
-        self._books = books
-        self._booklist = booklist
+        self._books = 0
+        self._booklist = []
 
     def getname(self):
         return self._name
@@ -69,7 +69,7 @@ class Patron:
             self._books += 1
             self._booklist.append(book)
             book.setreader(self._name)
-            print(self.getname(), "borrows", book.gettitle())
+            print(self.getname(), "借阅了", book.gettitle())
         else:
             if book.getwaitreader()[0] is self:
                 self._books += 1
@@ -101,26 +101,62 @@ class Patron:
         return self._name
 
 
-# class Library:
-#     """图书馆类"""
-#     def __init__(self,booklist = [], patronlist = []):
-#         """构造函数"""
-#         self._booklist = booklist
-#         self._patronlist = patronlist
+class Library:
+    """图书馆类"""
+    def __init__(self):
+        """构造函数"""
+        self._booklist = []
+        self._patronlist = []
 
-#     def addbook(self, book):
-#         """添加图书"""
-#         if book in self._booklist:
-#             print('此书已经添加，不需要重复添加')
-#         else:
-#             self._booklist.append(book)
+    def addbook(self, book):
+        """添加图书"""
+        if book in self._booklist:
+            print('此书已经添加，不需要重复添加')
+        else:
+            self._booklist.append(book)
 
-#     def addpatron(self, patron):
-#         """添加读者"""
-#         if patron in self._patronlist:
-#             print('此读者已经添加，不需要重复添加')
-#         else:
-#             self._patronlist.append(patron)
+    def addpatron(self, patron):
+        """添加读者"""
+        if patron in self._patronlist:
+            print('此读者已经添加，不需要重复添加')
+        else:
+            self._patronlist.append(patron)
+
+
+    def deletebook(self, book):
+        """删除图书"""
+        if book in self._booklist:
+            self._booklist.remove(book)
+            print("成功删除{0}".format(book.gettitle()))
+        else:
+            print("没有这本书，无法删除！")
+
+    def deletepatron(self, patron):
+        """删除读者"""
+        if patron in self._patronlist:
+            self._patronlist.remove(patron)
+            print("成功删除{0}".format(patron.getname()))
+        else:
+            print("没有这个读者，无法删除！")
+
+    def searchbooktitle(self,booktitle):
+        """通过书标题搜索图书"""
+        if booktitle in [x.gettitle() for x in self._booklist]:
+            for i in self._booklist:
+                if i.gettitle() == booktitle:
+                    print(i)
+        else:
+            print("没有这本书")
+
+    def searchpatronname(self,patronname):
+        """通过读者名字搜索读者"""
+        if patronname in [x.getname() for x in self._patronlist]:
+            for i in self._patronlist:
+                if i.getname() == patronname:
+                    print(i)
+        else:
+            print("没有这位读者")
+
 
 
 if __name__ == '__main__':
@@ -131,19 +167,29 @@ if __name__ == '__main__':
     p1 = Patron('张无忌')
     p2 = Patron('孙行者')
     p3 = Patron('钟无艳')
+    l = Library()
+    l.addbook(b1)
+    l.addbook(b2)
+    l.addbook(b3)
+    l.addbook(b4)
+    l.addpatron(p1)
+    l.addpatron(p2)
+    l.addpatron(p3)
     p1.borrow(b1)
     p1.borrow(b2)
     p1.borrow(b3)
     p2.borrow(b1)
     p3.borrow(b1)
+    l.searchbooktitle('语文')
+    l.searchpatronname('张无忌')
     p1.sendback(b1)
-    # p2.borrow(b1)
-    # p3.borrow(b1)
-    # p2.sendback(b1)
-    # p1.borrow(b1)
-    # p3.borrow(b1)
-    # p3.sendback(b1)
-    # p1.borrow(b1)
-    # p1.borrow(b4)
+    p2.borrow(b1)
+    p3.borrow(b1)
+    p2.sendback(b1)
+    p1.borrow(b1)
+    p3.borrow(b1)
+    p3.sendback(b1)
+    p1.borrow(b1)
+    p1.borrow(b4)
+    l.searchbooktitle('语文')
 
-    print(p1,'\n' ,b1)
