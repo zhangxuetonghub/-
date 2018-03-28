abff4591aeaea36f3600bf4baa3f4d3a4731624a
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-03-28 06:46:12 +0000

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,1 +1,26 @@
 def binarySearch(target, sortedLyst):
+    """二叉搜索"""
+    left = 0
+    right = len(sortedLyst) - 1
+    timer = 0
+    while left < right:
+        timer += 1
+        midpoint = (left + right) // 2
+        if target == sortedLyst[midpoint]:
+            return midpoint,timer
+        elif target < sortedLyst[midpoint]:
+            right = midpoint - 1
+        else:
+            left = midpoint + 1
+    return -1,timer
+
+if __name__ == '__main__':
+    l = list(range(1000000))
+    print(binarySearch(1000,l))
+
+# 可能比顺序搜索更高效，但是有一个额外的整体性代价，必须保持列表是有序的。最坏情况下循环的次数为 n/2/2... 最后商为1的次数 所以算法的复杂度为O(log2N)
+
+
+
+            
+        
