e3a6620f6a0220fa09ecc65bb81372609ce47ff9
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-03-28 09:00:00 +0000

    Update swap.py

--- sort/swap.py
+++ sort/swap.py
@@ -1,1 +1,13 @@
+def swap(lyst, i, j):
+    """交换列表中的两项的位置"""
+    lyst[i], lyst[j] = lyst[j], lyst[i]
+#     temp = lyst[i]
+#     lyst[i] = lyst[j]
+#     lyst[j] = temp
 
+# 为其他排序算法提供服务
+
+if __name__ == '__main__':
+    l = [1,2,3,4,5]
+    swap(l, 0, 4)
+    print(l)
