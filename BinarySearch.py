de59bce3bc5813370aa00c6a5e19316f20d974f6
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-03-28 08:18:08 +0000

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -12,9 +12,11 @@ def binarySearch(target, sortedLyst):
             left = midpoint + 1
     return -1
 
+
 if __name__ == '__main__':
-    l = list(range(1000000))
-    print(binarySearch(1000,l))
+    l = [20, 44, 48, 55, 62, 66, 74, 88, 93, 99]
+    print(binarySearch(44, l))
+
 
 # 可能比顺序搜索更高效，但是有一个额外的整体性代价，必须保持列表是有序的。最坏情况下循环的次数为 n/2/2... 最后商为1的次数 所以算法的复杂度为O(log2N)
 
