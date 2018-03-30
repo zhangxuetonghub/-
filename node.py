b5544aae6025721cbf04dde8e415a8596286bb9e
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-03-30 07:44:47 +0000

    Update node.py

--- node.py
+++ node.py
@@ -1,1 +1,15 @@
-fda
+class Node(object):
+    """定义一个单链表节点类"""
+    
+    def __init__(self, data, next = None):
+        """实例化一个节点使用默认的next为None"""
+        self.data = data
+        self.next = next
+        
+if __name__ == '__main__':
+    head = None
+    for count in range(1, 6):
+        head = Node(count, head)
+        
+    print(head.data)
+        
