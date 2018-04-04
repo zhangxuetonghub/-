e9ff7122671afaae1d3fa649987a9429c3196f1f
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-04-04 15:41:35 +0800

    Update try-except.py

--- First/try-except.py
+++ First/try-except.py
@@ -1,1 +1,20 @@
+"""
+捕获异常的示例
+"""
 
+def safeIntegerInput(propmt):
+    """
+    获取一个用户输入的整数值，如果用户输入错误的整数值，提示用户重新输入
+    """
+    inputString = input(propmt)
+    try:
+        number = int(inputString)
+        return number
+    except ValueError:
+        print("输入的整数值 {0} 有误，请重新输入".format(inputString))
+        return safeIntegerInput(propmt)
+
+
+if __name__ == "__main__":
+    i = safeIntegerInput("请输入你的年龄：")
+print(i)
