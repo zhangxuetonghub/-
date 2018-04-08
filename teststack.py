84f1b5c9ece8f2c01983ac77d05ec55e7c99d245
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-04-08 01:33:37 +0000

    Update teststack.py

--- teststack.py
+++ teststack.py
@@ -1,2 +1,29 @@
 from arrarstack import ArrayStack
 from linkedstack import LinkedStack
+
+def test(stackType):
+    """stack测试函数"""
+    s = stackType()
+    print("Length:", len(s))
+    print('Empty:', s.isEmpty())
+    print('Push 1-10')
+    for i in range(10):
+        s.push(i + 1)
+    print('Peeking:', s.peek())
+    print('Items (bottom to top):', s)
+    print('Length', len(s))
+    print('Empty:', s.isEmpty())
+    theClone = stackType(s)
+    print('Items in clone (bottom to top):', theClone)
+    theClone.clear()
+    print('Length of clone after clear:', len(theClone))
+    print('Push 11')
+    s.push(11)
+    print('Popping items (top to bottom):', end='')
+    while not s.isEmpty():print(s.pop, end=' ')
+    print('\nLength:', len(s))
+    print('Empty:', s.isEmpty())
+    
+test(ArrayStack)
+test(LinkedStack)
+        
